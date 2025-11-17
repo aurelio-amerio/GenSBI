7fa1b3d7e7eb55697ad474b23e1c34dd35e115ad
Aurelio Amerio <aure.amerio@gmail.com>
2025-11-17 17:14:42 +0100

    updated docstrings for new pipelines

--- src/gensbi/recipes/flux1.py
+++ src/gensbi/recipes/flux1.py
@@ -3,7 +3,7 @@ Pipeline for training and using a Flux1 model for simulation-based inference.
 
 Example:
     .. code-block:: python
-        import itertools
+        import grain
         import jax
         from jax import numpy as jnp
         from gensbi.recipes import Flux1Pipeline
@@ -14,17 +14,32 @@ Example:
 
         batch_size = 32
 
-        train_batch = train_data.reshape(-1, batch_size, train_data.shape[-1])
-        val_batch = val_data.reshape(-1, batch_size, val_data.shape[-1])
+        train_dataset_grain = (
+            grain.MapDataset.source(np.array(train_data)[...,None])
+            .shuffle(42)
+            .repeat()
+            .to_iter_dataset()
+            .batch(batch_size)
+            # .mp_prefetch() # Uncomment if you want to use multiprocessing prefetching
+        )
 
-        # Create datasets iterators (in this case with itertools, although a grain dataset is recommended)
-        train_dataset = itertools.cycle(train_batch)
-        val_dataset = itertools.cycle(val_batch)
+        val_dataset_grain = (
+            grain.MapDataset.source(np.array(val_data)[...,None])
+            .shuffle(42)
+            .repeat()
+            .to_iter_dataset()
+            .batch(batch_size) 
+            # .mp_prefetch() # Uncomment if you want to use multiprocessing prefetching
+        )
+        
+        .. note::
+        If you plan on using multiprocessing prefetching, ensure that your script is wrapped in a `if __name__ == "__main__":` guard. See https://docs.python.org/3/library/multiprocessing.html
+
 
         # Define the model
         dim_theta = 2  # Dimension of the parameter space
         dim_x = 2      # Dimension of the observation space
-        pipeline = Flux1Pipeline(train_dataset, val_dataset, dim_theta, dim_x)
+        pipeline = Flux1Pipeline(train_dataset_grain, val_dataset_grain, dim_theta, dim_x)
 
         # Train the model
         rngs = jax.random.PRNGKey(0)
--- src/gensbi/recipes/unconditional_pipeline.py
+++ src/gensbi/recipes/unconditional_pipeline.py
@@ -3,7 +3,7 @@ Pipeline for training and using a Unconditional model for simulation-based infer
 
 Example:
     .. code-block:: python
-        import itertools
+        import grain
         import jax
         from jax import numpy as jnp
         from gensbi.recipes import UnconditionalPipeline
@@ -14,13 +14,27 @@ Example:
 
         batch_size = 32
 
-        train_batch = train_data.reshape(-1, batch_size, train_data.shape[-1])
-        val_batch = val_data.reshape(-1, batch_size, val_data.shape[-1])
+        train_dataset_grain = (
+            grain.MapDataset.source(np.array(train_data)[...,None])
+            .shuffle(42)
+            .repeat()
+            .to_iter_dataset()
+            .batch(batch_size)
+            # .mp_prefetch() # Uncomment if you want to use multiprocessing prefetching
+        )
 
-        # Create datasets iterators (in this case with itertools, although a grain dataset is recommended)
-        train_dataset = itertools.cycle(train_batch)
-        val_dataset = itertools.cycle(val_batch)
+        val_dataset_grain = (
+            grain.MapDataset.source(np.array(val_data)[...,None])
+            .shuffle(42)
+            .repeat()
+            .to_iter_dataset()
+            .batch(batch_size) 
+            # .mp_prefetch() # Uncomment if you want to use multiprocessing prefetching
+        )
         
+        .. note::
+        If you plan on using multiprocessing prefetching, ensure that your script is wrapped in a `if __name__ == "__main__":` guard. See https://docs.python.org/3/library/multiprocessing.html
+
         # Define your model 
         model = ...  # your nnx.Module model here, e.g., a simple MLP, or the Simformer model
         # if you define a custom model, it should take as input the following arguments:
@@ -32,19 +46,15 @@ Example:
         
         # the obs input should have shape (batch_size, dim_joint, c), and the output will be of the same shape
 
-    
-
         # Define the model
         dim_theta = 2  # Dimension of the parameter space
-        dim_x = 2      # Dimension of the observation space
-        pipeline = UnconditionalPipeline(model, train_dataset, val_dataset, dim_theta, dim_x)
+        pipeline = UnconditionalPipeline(model, train_dataset_grain, val_dataset_grain, dim_theta)
 
         # Train the model
         rngs = jax.random.PRNGKey(0)
         pipeline.train(rngs)
 
         # Sample from the posterior
-        x_o = jnp.array([0.5, -0.2])  # Example
         samples = pipeline.sample(rngs, nsamples=10000, step_size=0.01)
 
 """
