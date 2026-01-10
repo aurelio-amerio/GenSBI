1035bfaf10e9c053c189b540f5e414e705ae0e18
Aurelio Amerio <aure.amerio@gmail.com>
2026-01-10 17:51:48 +0100

    up

--- test/diagnostics/test_distribution_wrapper.py
+++ test/diagnostics/test_distribution_wrapper.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import os
 
 os.environ["JAX_PLATFORMS"] = "cpu"
@@ -12,7 +12,8 @@ import pytest
 
 from gensbi.diagnostics import PosteriorWrapper
 
-#%%
+
+# %%
 class MockPipeline:
     def __init__(
         self,
@@ -39,20 +40,55 @@ class MockPipeline:
         shape = (nsamples, cond.shape[0], self.dim_obs, self.ch_obs)
         return jnp.zeros(shape)
 
-#%%   
+
+# %%
+
+
+def test_posterior_wrapper_processing():
+    pipeline = MockPipeline()
+    wrapper = PosteriorWrapper(pipeline, rngs=nnx.Rngs(0))
+
+    xs = jnp.ones((10, pipeline.dim_cond, pipeline.ch_cond))
+    xs_raveled = wrapper._ravel(jnp.array(xs))
+
+    assert xs_raveled.shape == (10, pipeline.dim_cond * pipeline.ch_cond)
+
+    xs_unraveled = wrapper._unravel_xs(xs_raveled)
+    assert xs_unraveled.shape == (10, pipeline.dim_cond, pipeline.ch_cond)
+
+    theta = jnp.ones((15, pipeline.dim_obs, pipeline.ch_obs))
+    theta_raveled = wrapper._ravel(jnp.array(theta))
+    assert theta_raveled.shape == (15, pipeline.dim_obs * pipeline.ch_obs)
+
+    theta_unraveled = wrapper._unravel_theta(theta_raveled)
+    assert theta_unraveled.shape == (15, pipeline.dim_obs, pipeline.ch_obs)
+
+    xs = jnp.ones((20, pipeline.dim_cond * pipeline.ch_cond))
+    xs_processed = wrapper._process_x(jnp.array(xs))
+    assert xs_processed.shape == (20, pipeline.dim_cond * pipeline.ch_cond)
+    xs = jnp.ones((25, pipeline.dim_cond, pipeline.ch_cond))
+    xs_processed_2 = wrapper._process_x(jnp.array(xs))
+    assert xs_processed_2.shape == (25, pipeline.dim_cond * pipeline.ch_cond)
+
+    return
+
+
 def test_distribution_wrapper_sampling():
     pipeline = MockPipeline()
     wrapper = PosteriorWrapper(pipeline, rngs=nnx.Rngs(0))
 
     # Test single sampling
-    samples = wrapper.sample(sample_shape=(4,), x = jnp.zeros((1, 4, 3)))
+    samples = wrapper.sample(sample_shape=(4,), x=jnp.zeros((1, 4, 3)))
 
-    assert samples.shape == (4, pipeline.dim_obs*pipeline.ch_obs), f"Unexpected shape: {samples.shape}"
+    assert samples.shape == (
+        4,
+        pipeline.dim_obs * pipeline.ch_obs,
+    ), f"Unexpected shape: {samples.shape}"
 
     # Test batched sampling
-    samples_batched = wrapper.sample_batched(
-        sample_shape=(5,),
-        x = jnp.zeros((2, 4, 3))
-    )
-    assert samples_batched.shape == (5, 2, pipeline.dim_obs*pipeline.ch_obs), f"Unexpected shape: {samples_batched.shape}"
-
+    samples_batched = wrapper.sample_batched(sample_shape=(5,), x=jnp.zeros((2, 4, 3)))
+    assert samples_batched.shape == (
+        5,
+        2,
+        pipeline.dim_obs * pipeline.ch_obs,
+    ), f"Unexpected shape: {samples_batched.shape}"
