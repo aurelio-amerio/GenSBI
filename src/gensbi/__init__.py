e7f18fbd5887675f79214aa01c2ea38ac20a2aff
Aurelio Amerio <aure.amerio@gmail.com>
2026-01-13 21:36:59 +0100

    up

--- src/gensbi/__init__.py
+++ src/gensbi/__init__.py
@@ -12,6 +12,14 @@ See the documentation for details and usage examples.
 
 __version__ = "0.0.1"
 
+import warnings
+
+# Suppress protobuf runtime version warning (grain dependency)
+warnings.filterwarnings(
+    "ignore", category=UserWarning, module="google.protobuf.runtime_version"
+)
+
+
 # coverage report:
 # diffusion: 100%
 # flow_matching: 100%
